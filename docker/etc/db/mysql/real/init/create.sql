bee5bcc662819997de53694d670fb5b823fe4ffd
Cayetano H. Osma <moesis@gmail.com>
2024-07-27 19:28:30 +0100

    Cleanup in docker Fix some issues with docker environment

--- docker/etc/db/mysql/real/init/create.sql
+++
@@ -1,6 +0,0 @@
--- Normal database
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
-CREATE DATABASE IF NOT EXISTS `egatebe` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `dbuser`@`%` IDENTIFIED BY 'dbuser';
-GRANT ALL PRIVILEGES ON egatebe.* TO `dbuser`@`%`;
-FLUSH PRIVILEGES;
